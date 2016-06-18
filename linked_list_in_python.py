e098cf078b8a00705c50231fd8a2ee2a45f8ea9e
chenglinguang <clg19881220@163.com>
2016-06-18 23:49:59 +0800

    define a linded list in python

---
+++ linked_list_in_python.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3 
+
+#-*- coding:utf-8 -*-
+
+class Node:
+    def __init__(self,data)
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+
+    def AddNode(self,data):
+        pass
+    
+    def RemoveNode(self,index):
+        pass
+
+    def PrintList(self):
+        pass 
+
+
