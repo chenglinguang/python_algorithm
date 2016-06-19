096e3a441e2a436c09bb4c11d75eb0013fba4ce7
chenglinguang <clg19881220@163.com>
2016-06-19 22:16:16 +0800

    add some practice

--- ListNode.py
+++ ListNode.py
@@ -9,21 +9,40 @@ define a listNode class , each node will have a next pointer which will point to
 class listNode(object):
     #When started to define a listNode, the next pointer refers to None
     def __init__(self,data):
-        self.val=data
-        self.next=None
+        self._val=data
+        self._next=None
     
     @property
     def val(self):
-        return self.val
+        return self._val
     
     @property
     def next(self):
-        return self.next
+        return self._next
 
     @val.setter
     def val(self,data):
-        self.val=data
+        self._val=data
 
     @next.setter
     def next(self,data):
-        self.next=data
+        self._next=data
+
+
+
+listnode=listNode(1)
+listnode.next=5
+print(listnode.val)
+print(listnode.next)
+
+
+
+
+
+
+
+
+
+
+
+
