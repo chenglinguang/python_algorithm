41c99cf4d2e68f5d19407e54493b323d43dd9622
chenglinguang <clg19881220@163.com>
2016-06-28 14:56:55 +0800

    triandles

--- yanghuisanjiao.py
+++ yanghuisanjiao.py
@@ -10,12 +10,13 @@ def triangles():
         N=[N[i-1]+N[i] for i in range(len(N))]
 
 
-n=0
-for t in triangles():
-    print(t)
-    n=n+1
-    if n ==10:
-        break
+if __name__=='__main__':
+    n=0
+    for t in triangles():
+        print(t)
+        n=n+1
+        if n ==10:
+            break
 
 
 
