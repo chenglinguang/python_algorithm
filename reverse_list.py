4ce9cf5d88c88d96cb0af93da39a62380448e59d
chenglinguang <clg19881220@163.com>
2016-07-11 11:22:33 +0800

    reverse a list in two methods

--- reverse_list.py
+++ reverse_list.py
@@ -4,7 +4,26 @@
 
 l1=['A','B','C']
 
-class Solution(object):
-    def get_reverse(ll):
-         length=len(ll)
-          
+
+def get_reverse_1(llist):
+    new=[]
+    if llist:
+        length=len(llist)
+        for i in range(length):
+            new.append(llist[length-i-1])
+        return new
+ 
+def get_reverse_2(llist):
+    llist.reverse()
+    return llist
+
+
+
+
+                 
+print(get_reverse_1(l1))
+print(get_reverse_2(l1))
+
+
+
+ 
