f8924ce3a44a45c2da550a2d2d4f44571b65277e
chenglinguang <clg19881220@163.com>
2016-07-21 16:21:19 +0800

    add some coments

--- sorts/sorts.py
+++ sorts/sorts.py
@@ -43,29 +43,31 @@ def updated_bubble_sort(lst):
 
 
 #print(updated_bubble_sort(lst))
+#快速排序算法
 def quick_sort(lst):
     qsort_rec(lst,0,len(lst)-1)
-    
+
+#递归函数    
 def qsort_rec(lst,a,b):
     if a>=b:
-        return 
+        return                            #分段无记录或者只有一个记录
     i=a
     j=b
-    pivot=lst[i]
-    while i<j:
-        while i<j and lst[j]>=pivot:
-            j-=1
+    pivot=lst[i]                          #初始空位
+    while i<j:                            #寻找pivot的最终位置
+        while i<j and lst[j]>=pivot:      
+            j-=1                          #j向左扫描寻找小于pivot的记录
         if i<j:
             lst[i]=lst[j]
-            i+=1
-        while i<j and lst[i]<=pivot:
-            i+=1
+            i+=1                          #小记录移到左边
+        while i<j and lst[i]<=pivot:       
+            i+=1                          #i向右扫描找大于pivot的记录
         if i<j:
             lst[j]=lst[i]
-            j-=1
-    lst[i]=pivot
-    qsort_rec(lst,a,i-1)
-    qsort_rec(lst,i+1,b)
+            j-=1                          #大记录移动到右边
+    lst[i]=pivot                          #讲pivot存入其最终位置
+    qsort_rec(lst,a,i-1)                  #递归处理左半区
+    qsort_rec(lst,i+1,b)                  #递归处理右半区
 
 #quick_sort(lst)
 #print(lst)
